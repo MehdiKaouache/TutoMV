8a24dd375d419397536a44b5e53136f89ecbc701
Kalem9090 <merouihamza@gmail.com>
2026-03-25 18:22:39 -0400

    TVM-7 #done Ajout ProfileViewModel et mise à jour AppUser avec Nom, Prenom, Role

--- Projet-Session-Entreprise/Projet-Session-Entreprise/AppUser.cs
+++ Projet-Session-Entreprise/Projet-Session-Entreprise/AppUser.cs
@@ -6,12 +6,18 @@
 
         public string DA { get; set; } = "";
 
+        public string Nom { get; set; } = "";
+
+        public string Prenom { get; set; } = "";
+
+        public string Role { get; set; } = "Étudiant";
+
         public string Password { get; set; } = "";
 
         public double AverageGrade { get; set; }
-        
+
         public bool HasSignedEngagement { get; set; }
-        
+
         public bool IsTutor { get; set; }
     }
 }
