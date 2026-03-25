fa40822d13b6c6a5405de900ee0729381b72bf45
Mikhaël François <mikhael.francois@gmail.com>
2026-03-24 21:51:15 -0400

    commit avant de changer de branche

--- Projet-Session-Entreprise/Projet-Session-Entreprise/obj/Debug/net8.0-windows/Projet-Session-Entreprise.AssemblyInfo.cs
+++ Projet-Session-Entreprise/Projet-Session-Entreprise/obj/Debug/net8.0-windows/Projet-Session-Entreprise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projet-Session-Entreprise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27563a9ce5ad1603e3de351bfcc50857f7cd6fb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0e446de0ecdccdccc5a5fc0261695158616fc09")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projet-Session-Entreprise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projet-Session-Entreprise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
