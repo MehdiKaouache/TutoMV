cf4ea55d1803cb00fe4a39d3ccb299a4ac543e11
Mikhaël François <mikhael.francois@gmail.com>
2026-03-24 21:44:11 -0400

    Vues dans le dossier Views, pré-data binding

--- Projet-Session-Entreprise/Projet-Session-Entreprise/obj/Debug/net8.0-windows/LoginWindow.g.i.cs
+++ Projet-Session-Entreprise/Projet-Session-Entreprise/obj/Debug/net8.0-windows/LoginWindow.g.i.cs
@@ -62,13 +62,13 @@ namespace Projet_Session_Entreprise {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.14.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.2.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Projet-Session-Entreprise;component/loginwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Projet-Session-Entreprise;V1.0.0.0;component/loginwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\LoginWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -78,7 +78,7 @@ namespace Projet_Session_Entreprise {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.14.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.2.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
--- Projet-Session-Entreprise/Projet-Session-Entreprise/obj/Debug/net8.0-windows/Projet-Session-Entreprise.AssemblyInfo.cs
+++ Projet-Session-Entreprise/Projet-Session-Entreprise/obj/Debug/net8.0-windows/Projet-Session-Entreprise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projet-Session-Entreprise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e15fd4aa7e0658855d0babac78f97d282513416")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e51b9170e724074a52a146412a7bd1076f4956a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projet-Session-Entreprise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projet-Session-Entreprise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
