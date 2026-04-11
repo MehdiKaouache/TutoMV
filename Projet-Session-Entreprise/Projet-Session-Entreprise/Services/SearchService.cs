8753cc528b974bfe3ae586dbf685183fb0e0c5c8
Kalem9090 <merouihamza@gmail.com>
2026-04-11 12:43:39 -0400

    TVM-9 #done Implémentation du SearchService pour la recherche publique de tuteurs

---
+++ Projet-Session-Entreprise/Projet-Session-Entreprise/Services/SearchService.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Projet_Session_Entreprise.Services
+{
+    public class SearchService
+    {
+        private readonly AppDbContext _db;
+
+        public SearchService(AppDbContext db) => _db = db;
+
+        public async Task<List<Tutor>> SearchByNameAsync(string nom)
+        {
+            return await _db.Tutors
+                .Where(t => t.IsValidated &&
+                       (t.Nom.ToLower().Contains(nom.ToLower()) || t.Prenom.ToLower().Contains(nom.ToLower())))
+                .ToListAsync();
+        }
+
+        public async Task<List<Tutor>> SearchBySubjectAsync(string matiere)
+        {
+            return await _db.Tutors
+                .Where(t => t.IsValidated && t.Subject.ToLower().Contains(matiere.ToLower()))
+                .ToListAsync();
+        }
+    }
+}
