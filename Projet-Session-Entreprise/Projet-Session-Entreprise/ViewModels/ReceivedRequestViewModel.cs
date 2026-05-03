8287cb0ebd0d0ad622826bbfacdc999c6a7e933e
Mathieu Joseph <6261308@cegepmv.ca>
2026-05-03 18:07:50 -0400

    TVM-15 #done Gestion des statuts de demandes de rendez-vous

---
+++ Projet-Session-Entreprise/Projet-Session-Entreprise/ViewModels/ReceivedRequestViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using Projet_Session_Entreprise.Models;
+
+namespace Projet_Session_Entreprise.ViewModels
+{
+    public partial class ReceivedRequestViewModel : ObservableObject
+    {
+        private readonly Tutor _tutor;
+
+        public ObservableCollection<Appointment> Appointments { get; set; } = new();
+
+        public ReceivedRequestViewModel(Tutor tutor)
+        {
+            _tutor = tutor;
+            LoadAppointments();
+        }
+
+        private void LoadAppointments()
+        {
+            using (var db = new AppDbContext())
+            {
+                var list = db.Appointments.Where(a => a.TutorId == _tutor.Id).ToList();
+
+                Appointments.Clear();
+
+                foreach (var a in list)
+                {
+                    Appointments.Add(a);
+                }
+            }
+        }
+
+        [RelayCommand]
+        private void Accepter(Appointment appointment)
+        {
+            using (var db = new AppDbContext())
+            {
+                var a = db.Appointments.Find(appointment.Id);
+                if (a != null)
+                {
+                    a.Status = "Accepté";
+                    db.SaveChanges();
+                }
+            }
+
+            LoadAppointments();
+        }
+
+        [RelayCommand]
+        private void Refuser(Appointment appointment)
+        {
+            using (var db = new AppDbContext())
+            {
+                var a = db.Appointments.Find(appointment.Id);
+                if (a != null)
+                {
+                    a.Status = "Refusé";
+                    db.SaveChanges();
+                }
+            }
+            LoadAppointments();
+        }
+
+    }
+}
